7bf3aab8eb7043a42e6c5dfdfdbdf8c8e7a1b5ff
samukada <95943966+samukada@users.noreply.github.com>
2025-05-14 10:28:32 -0700

    Update LockFragment.kt

--- app/src/main/java/com/example/appblock/lock/LockFragment.kt
+++ app/src/main/java/com/example/appblock/lock/LockFragment.kt
@@ -9,7 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
-import com.example.appblock.BlockedAppsActivity
+// import com.example.appblock.BlockedAppsActivity
 import com.example.appblock.MonitoredAppsActivity
 import com.example.appblock.R
 
